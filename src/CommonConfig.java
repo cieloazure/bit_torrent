b59f3d98c9a09aa5b9ef4b926caef84397bd1ceb
Akash S <akash.shingte@gmail.com>
2019-03-12 21:45:34 -0400

    WIP multiple peers

---
+++ src/CommonConfig.java
@@ -0,0 +1,8 @@
+public class Config {
+    private static int numOfPreferredNeighbours;
+    private static int unchokingInterval;
+    private static int optimisticUnchokingInterval;
+    private static String fileName;
+    private static long fileSize;
+    private static long pieceSize;
+}
