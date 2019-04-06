19880855de67dfe1d52e0ad7287cb082a299d207
Sharmilee <sharmileedesai@ufl.edu>
2019-04-06 17:12:59 -0400

    state transitions

--- src/ExpectedToSendInterestedOrNotInterestedMessageState.java
+++ src/ExpectedToSendInterestedOrNotInterestedMessageState.java
@@ -25,12 +25,68 @@ public class ExpectedToSendInterestedOrNotInterestedMessageState implements Peer
 
                 outputStream.write(actualMessage.serialize());
                 outputStream.flush();
+
+                switch (neighbourConnectionInfo.get(context.getTheirPeerId()).getNeighbourState()) {
+
+                    case UNKNOWN:
+                        neighbourConnectionInfo.get(context.getTheirPeerId()).setNeighbourState(NeighbourState.NOT_INTERESTED);
+                        break;
+                    case CHOKED:
+                        neighbourConnectionInfo.get(context.getTheirPeerId()).setNeighbourState(NeighbourState.CHOKED_AND_NOT_INTERESTED);
+                        break;
+                    case INTERESTED:
+                        neighbourConnectionInfo.get(context.getTheirPeerId()).setNeighbourState(NeighbourState.NOT_INTERESTED);
+                        break;
+                    case UNCHOKED:
+                        //stays choked
+                        neighbourConnectionInfo.get(context.getTheirPeerId()).setNeighbourState(NeighbourState.CHOKED_AND_NOT_INTERESTED);
+                        break;
+                    case UNCHOKED_AND_INTERESTED:
+                        neighbourConnectionInfo.get(context.getTheirPeerId()).setNeighbourState(NeighbourState.CHOKED_AND_NOT_INTERESTED);
+                        break;
+                    case CHOKED_AND_INTERESTED:
+                        neighbourConnectionInfo.get(context.getTheirPeerId()).setNeighbourState(NeighbourState.CHOKED_AND_NOT_INTERESTED);
+                        break;
+                    case CHOKED_AND_NOT_INTERESTED:
+                        break;
+                    case NOT_INTERESTED:
+                        break;
+
+                }
+
+
             } else {
                 ActualMessage actualMessage = new ActualMessage(MessageType.INTERESTED);
                 myPeerInfo.log( "[PEER:" + myPeerInfo.getPeerID() + "]Sent INTERESTED message to " + context.getTheirPeerId());
 
                 outputStream.write(actualMessage.serialize());
                 outputStream.flush();
+                switch (neighbourConnectionInfo.get(context.getTheirPeerId()).getNeighbourState()) {
+
+                    case UNKNOWN:
+                        neighbourConnectionInfo.get(context.getTheirPeerId()).setNeighbourState(NeighbourState.INTERESTED);
+                        break;
+                    case CHOKED:
+                        neighbourConnectionInfo.get(context.getTheirPeerId()).setNeighbourState(NeighbourState.CHOKED_AND_INTERESTED);
+                        break;
+                    case NOT_INTERESTED:
+                        neighbourConnectionInfo.get(context.getTheirPeerId()).setNeighbourState(NeighbourState.INTERESTED);
+                        break;
+                    case UNCHOKED:
+                        //stays choked
+                        neighbourConnectionInfo.get(context.getTheirPeerId()).setNeighbourState(NeighbourState.UNCHOKED_AND_INTERESTED);
+                        break;
+                    case CHOKED_AND_NOT_INTERESTED:
+                        neighbourConnectionInfo.get(context.getTheirPeerId()).setNeighbourState(NeighbourState.CHOKED_AND_INTERESTED);
+                        break;
+                    case UNCHOKED_AND_INTERESTED:
+                        break;
+                    case CHOKED_AND_INTERESTED:
+                        break;
+                    case INTERESTED:
+                        break;
+
+                }
             }
 
             if (this.setState) {
